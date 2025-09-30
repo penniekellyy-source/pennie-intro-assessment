8de441f847b4ea47629553a646ff8a4dc5592ede
penniekellyy-source <penniekellyy@gmail.com>
2025-09-30 14:14:59 -0500

    made rooms

--- Game.cpp
+++ Game.cpp
@@ -18,15 +18,31 @@ Room::Room(string name, string desc)
 	roomDescription = desc;
 }
 
+Room::Room(string name, string desc, int setNorth, int setSouth, int setEast, int setWest)
+{
+	roomName = name;
+	roomDescription = desc;
+
+	north = setNorth;
+	south = setSouth;
+	east = setEast;
+	west = setWest;
+}
+
 void Room::enterRoom() const // prints room info
 {
 	cout << roomName << endl;
 	cout << roomDescription << endl << endl;
 }
 
-void roomSetup(Room rooms[], int size)
+Locations::Locations()
 {
-	if (size < 4) return; // stopping the function early if size exceeds amount of rooms
+	Locations(4);
+}
+
+Locations::Locations(int size)
+{
+	if (size > 4) return; // stopping the function early if size exceeds amount of rooms
 
 	rooms[0] = Room("North Room", "This is the north room"); // object array, using custom constructor in room class, name & desc
 	rooms[1] = Room("South Room", "This is the south room"); // placeholder names & descs
@@ -34,10 +50,12 @@ void roomSetup(Room rooms[], int size)
 	rooms[3] = Room("West Room", "This is the west room");
 }
 
-void playGame(Room rooms[], int size)
+void Player::playGame()
 {
-	int currentRoom = 0;
 	string command;
+	getline(cin, command);
+	stringUtil util;
+	int currentRoom = 0;
 
 	cout << "Adventure Game Test" << endl;
 	cout << "Available Directions :" << endl;
@@ -46,21 +64,29 @@ void playGame(Room rooms[], int size)
 	cout << "Where will you go?" << endl;
 	cin >> command;
 
-	if (command == "north")
+	if (util.toUpper(command) == "GO NORTH")
 	{
 		currentRoom = 0;
+		cout << locations.rooms[0].roomDescription << endl; // grabbing array of rooms from locations class stored in Player & its description
+		playGame();
 	}
-	else if (command == "south")
+	else if (util.toUpper(command) == "GO SOUTH")
 	{
 		currentRoom = 1;
+		cout << locations.rooms[1].roomDescription << endl;
+		playGame();
 	}
-	else if (command == "east")
+	else if (util.toUpper(command) == "GO EAST")
 	{
 		currentRoom = 2;
+		cout << locations.rooms[2].roomDescription << endl;
+		playGame();
 	}
-	else if (command == "west")
+	else if (util.toUpper(command) == "GO WEST")
 	{
 		currentRoom = 3;
+		cout << locations.rooms[3].roomDescription << endl;
+		playGame();
 	}
 	else
 	{
@@ -68,4 +94,12 @@ void playGame(Room rooms[], int size)
 	}
 }
 
+Player::Player()
+{
 
+}
+
+Locations::Locations()
+{
+
+}
--- Game.h
+++ Game.h
@@ -5,12 +5,33 @@
 
 using namespace std;
 
+class Entity
+{
+protected:
+
+
+public:
+
+	int currentRoom;
+
+};
+
+class Player : Entity
+{
+public:
+
+	void playGame();
+
+	Player();
+	void playerChoice();
+
+	Locations locations;
+
+};
+
 class Room
 {
 private:
-
-	string roomName;
-	string roomDescription;
 
 	int north; 
 	int south;
@@ -19,12 +40,26 @@ private:
 
 public:
 
+	string roomName;
+	string roomDescription;
+
 	Room(); // default constructor
 
 	Room(string roomName, string roomDescription); // custom constructor
 
+	Room(string roomName, string roomDescription, int setNorth, int setSouth, int setEast, int setWest);
+
 	void enterRoom() const; // const to make sure room name can't be accidentally changed
 
 };
-void roomSetup(Room rooms[], int size);
+
+class Locations : Room
+{
+public:
+	
+	Locations();
+	Locations(int size);
+	Room rooms[4];
+};
+
 void playGame(Room rooms[], int size);
