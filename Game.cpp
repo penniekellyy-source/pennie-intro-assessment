668ac7532ade230975787ca7fa9444dd0595784f
penniekellyy-source <penniekellyy@gmail.com>
2025-09-30 09:35:55 -0500

    room setup
    okay

--- Game.cpp
+++ Game.cpp
@@ -10,21 +10,39 @@ Room::Room()
 {
 	name = " ";
 	description = " ";
+
+	north = -1; // initialize variables in constructor
+	south = -1;
+	east = -1;
+	west = -1;
 }
 
 Room::Room(string roomName, string desc)
 {
 	name = roomName;
 	desc = description;
+
+	north = -1; // since arrays start at 0, -1 means "this room doesn't exist"
+	south = -1;
+	east = -1;
+	west = -1;
 }
 
 void Room::enterRoom() const
 {
 	cout << name << endl;
 	cout << description << endl << endl;
+
+
 }
 
 void roomSetup(Room rooms[], int size)
 {
-	if (size < 3) return; // stopping the function early
+	if (size < 4) return; // stopping the function early if size exceeds amount of rooms
+
+	rooms[0] = Room(" ", " "); // object array, using custom constructor in room class, name & desc
+	rooms[1] = Room(" ", " ");
+	rooms[2] = Room(" ", " ");
+	rooms[3] = Room(" ", " ");
 }
+
--- Game.h
+++ Game.h
@@ -12,11 +12,10 @@ private:
 	string name;
 	string description;
 
-
-	int north = -1; // since arrays start at 0, -1 means "This room doesn't exist"
-	int south = -1;
-	int east = -1;
-	int west = -1;
+	int north; 
+	int south;
+	int east;
+	int west;
 
 public:
 
@@ -25,5 +24,5 @@ public:
 	Room(string name, string description); // custom constructor
 
 	void enterRoom() const; // const to make sure room name can't be accidentally changed
-    //
 };
+
